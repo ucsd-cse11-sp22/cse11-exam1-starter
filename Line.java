676967c1b269e7f153635f6d7f82c9e6c82e0260
Greg Miranda <g2miranda@ucsd.edu>
2022-04-14 13:45:43 -0700

    Initial checkin

---
+++ Line.java
@@ -0,0 +1,12 @@
+class Line {
+    
+    // Task 1: computeY method
+    // Your code here
+
+    // Task 1: calculateSlope method
+    // Your code here
+    
+    // Task 1: Add tests here
+    // Your code here
+
+}
